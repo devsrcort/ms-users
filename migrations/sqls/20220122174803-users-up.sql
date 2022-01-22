67facc5c51b2936a5bcf269787b4d9d7401f61cf
WYS <silence14@hanmail.net>
2022-01-23 04:30:05 +0900

    Initialize user service

---
+++ migrations/sqls/20220122174803-users-up.sql
@@ -0,0 +1,16 @@
+/* Replace with your SQL commands */
+
+
+CREATE TABLE `users` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(36) NOT NULL DEFAULT '',
+    `phonenumer` varchar(36) NOT NULL DEFAULT '',
+    `email` varchar(1000) NOT NULL DEFAULT '',
+    `password` varchar(1000) NOT NULL DEFAULT '',
+    `wallet_address` varchar(1000) NOT NULL DEFAULT '',
+    `balance` int(11) unsigned NOT NULL DEFAULT 0,
+    `withdraw_limit` int(11) unsigned NOT NULL DEFAULT 100,
+    `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_updated` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ migrations/sqls/20220122174815-sample-data-up.sql
@@ -0,0 +1,11 @@
+/* Replace with your SQL commands */
+
+
+INSERT INTO `users` (`id`, `name`, `phonenumer`, `email`, `password`, `wallet_address`, `balance`, `withdraw_limit`, `created`, `last_updated`)
+VALUES
+	(1, 'Kim', '01012341234', 'first@example.com', '', '0x348901dfad72831', 100, 50, '2017-03-20 01:28:20', '2017-04-18 01:30:58'),
+	(2, 'Lee', '01012344312', 'second@example.com', '', '0x348901dfad72832', 101, 60, '2017-04-10 01:28:42', '2017-04-10 01:28:42'),
+	(3, 'Han', '01043211234', 'kris@example.co', '', '0x348901dfad72833', 102, 70, '2017-04-15 01:28:59', '2017-04-15 01:28:59'),
+	(4, 'Min', '01012431234', 'jack@example.co.uk', '', '0x348901dfad72834', 103, 80, '2017-04-19 01:29:31', '2017-04-20 01:29:31'),
+	(5, 'June', '01032141234', 'molly@example.ca', '', '0x348901dfad72835', 104, 90, '2017-04-20 01:29:47', '2017-04-20 01:30:45');
+
