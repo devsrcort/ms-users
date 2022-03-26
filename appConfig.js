fe774537896daff24f4a75702c77553345e7de95
Dev <dev@srcorp.io>
2022-03-26 22:02:46 +0900

    Initialize DevJwt branch

--- appConfig.js
+++ appConfig.js
@@ -1,43 +1,51 @@
 // eslint-disable global-require
-const path = require('path');
-const helmet = require('helmet');
-const log = require('metalogger')();
-const healthcheck = require('maikai');
-const hbs = require('hbs');
-const cors = require('cors');
+const path = require("path");
+const helmet = require("helmet");
+const log = require("metalogger")();
+const healthcheck = require("maikai");
+const hbs = require("hbs");
+const cors = require("cors");
 
-require('app-module-path').addPath(path.join(__dirname, '/lib'));
+require("app-module-path").addPath(path.join(__dirname, "/lib"));
 
 // Add all routes and route-handlers for your service/app here:
 function serviceRoutes(app) {
-
     // For Liveness Probe, defaults may be all you need.
-    const livenessCheck = healthcheck({ "path": "/ping" });
+    const livenessCheck = healthcheck();
     app.use(livenessCheck.express());
 
     // For readiness check, let's also test the DB
-    const check = healthcheck();
-    const AdvancedHealthcheckers = require('healthchecks-advanced');
+    const check = healthcheck({ path: "/ping" });
+    const AdvancedHealthcheckers = require("healthchecks-advanced");
     const advCheckers = new AdvancedHealthcheckers();
     // Database health check is cached for 10000ms = 10 seconds!
-    check.addCheck('db', 'dbQuery', advCheckers.dbCheck, { minCacheMs: 10000 });
+    check.addCheck("db", "dbQueryCheck", advCheckers.dbQueryCheck, { minCacheMs: 10000 });
     app.use(check.express());
 
     /* eslint-disable global-require */
 
     // Temporary allow all urls
-    const safesitelist = ['https://srt-wallet.io', 'https://app.srt-wallet.io', 'https://admin.srt-wallet.io'];
+    const safesitelist =
+        process.env.NODE_ENV == "production" ? [
+            "https://srt-wallet.io",
+            "https://app.srt-wallet.io",
+            "https://admin.srt-wallet.io",
+        ] : [
+            "https://dev.srt-wallet.io",
+            "https://dev.app.srt-wallet.io",
+            "https://dev.admin.srt-wallet.io",
+        ];
 
     const corsOptions = {
         origin: function(origin, callback) {
             const issafesitelisted = safesitelist.indexOf(origin) !== -1;
             callback(null, issafesitelisted);
         },
-        credentials: true
+        credentials: true,
     };
 
     app.use(cors(corsOptions));
-    app.use('/users', require('users')); // attach to sub-route
+    app.use("/users", require("users")); // attach to sub-route
     // app.use('/register', require('users')); // attach to sub-route
 
     /* eslint-enable global-require */
@@ -48,7 +56,8 @@ function setupErrorHandling(app) {
     app.use((err, req, res, next) => {
         if (err) {
             const out = {};
-            if (err.isJoi || err.type === "validation") { //validation error. No need to log these
+            if (err.isJoi || err.type === "validation") {
+                //validation error. No need to log these
                 out.errors = err.details;
                 res.status(400).json(out);
                 return;
@@ -69,8 +78,8 @@ function setupErrorHandling(app) {
 
 exports.setup = function(app, callback) {
     // Choose your favorite view engine(s)
-    app.set('view engine', 'handlebars');
-    app.engine('handlebars', hbs.__express);
+    app.set("view engine", "handlebars");
+    app.engine("handlebars", hbs.__express);
 
     /** Adding security best-practices middleware
      * see: https://www.npmjs.com/package/helmet **/
@@ -86,7 +95,7 @@ exports.setup = function(app, callback) {
     // let socketio = require('socket.io')(runningApp.http);
     // require('fauxchatapp')(socketio);
 
-    if (typeof callback === 'function') {
+    if (typeof callback === "function") {
         callback(app);
         return;
     }
--- test/support/server.js
+++ test/support/server.js
@@ -1,16 +1,26 @@
-const server = require('nodebootstrap-server')
-    , express = require('express')
-    , healthcheck = require('maikai')
-    , appConfig = require('../../appConfig');
+const server = require('nodebootstrap-server'),
+    express = require('express'),
+    healthcheck = require('maikai'),
+    appConfig = require('../../appConfig');
 
 exports.beforeEach = function(app, callback) {
-  server.setupTest(app, function(app) {
-    app.use(healthcheck().express());
+    server.setupTest(app, function(app) {
+        // For Liveness Probe, defaults may be all you need.
+        const livenessCheck = healthcheck();
+        app.use(livenessCheck.express());
 
-    appConfig.setup(app, callback);
-  });
+        // For readiness check, let's also test the DB
+        const check = healthcheck({ path: "/ping" });
+        const AdvancedHealthcheckers = require("healthchecks-advanced");
+        const advCheckers = new AdvancedHealthcheckers();
+        // Database health check is cached for 10000ms = 10 seconds!
+        check.addCheck("db", "dbQueryCheck", advCheckers.dbQueryCheck, { minCacheMs: 10000 });
+        app.use(check.express());
+
+        appConfig.setup(app, callback);
+    });
 };
 
 exports.express = function() {
-  return express();
+    return express();
 };
