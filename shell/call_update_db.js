447dc51b8dd4c548286fe23c60b144b761cb0f15
Dev <dev@srcorp.io>
2022-06-29 17:48:04 +0900

    Change line feed

--- shell/call_update_db.js
+++ shell/call_update_db.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 
+
 // const res = async function() {
 //     await axios.get("https://app.srt-wallet.io/users/");
 // };
--- shell/start-dev.sh
+++ shell/start-dev.sh
@@ -2,3 +2,4 @@
 export NODE_ENV=dev
 export PORT=5501
 node_modules/db-migrate/bin/db-migrate up -e $NODE_ENV && nodemon -e js,handlebars server.js
+ 
