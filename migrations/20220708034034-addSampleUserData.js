51727976906e197b72a1cf122f40fbe3478b5381
Dev <dev@srcorp.io>
2022-07-08 20:52:47 +0900

    Update 20220708034034-addSampleUserData.js

--- migrations/20220708034034-addSampleUserData.js
+++ migrations/20220708034034-addSampleUserData.js
@@ -8,46 +8,56 @@ var path = require('path');
 var Promise;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
 exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
-  Promise = options.Promise;
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
+    Promise = options.Promise;
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20220708034034-addSampleUserData-up.sql');
-  return new Promise( function( resolve, reject ) {
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-      if (err) return reject(err);
-      console.log('received data: ' + data);
+    var filePath = path.join(__dirname, 'sqls', '20220708034034-addSampleUserData-up.sql');
+    const allowed = ["dev", "qa"];
+    if (!allowed.includes(db.internals.argv.env)) {
+        console.log(`Environment is not ${allowed}. Skipping ${filePath}`);
+        return new Promise(function(resolve, reject) { resolve(""); });
+    }
+    return new Promise(function(resolve, reject) {
+            fs.readFile(filePath, { encoding: 'utf-8' }, function(err, data) {
+                if (err) return reject(err);
+                console.log('received data: ' + data);
 
-      resolve(data);
-    });
-  })
-  .then(function(data) {
-    return db.runSql(data);
-  });
+                resolve(data);
+            });
+        })
+        .then(function(data) {
+            return db.runSql(data);
+        });
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20220708034034-addSampleUserData-down.sql');
-  return new Promise( function( resolve, reject ) {
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-      if (err) return reject(err);
-      console.log('received data: ' + data);
+    var filePath = path.join(__dirname, 'sqls', '20220708034034-addSampleUserData-down.sql');
+    const allowed = ["dev", "qa"];
+    if (!allowed.includes(db.internals.argv.env)) {
+        console.log(`Environment is not ${allowed}. Skipping ${filePath}`);
+        return new Promise(function(resolve, reject) { resolve(""); });
+    }
+    return new Promise(function(resolve, reject) {
+            fs.readFile(filePath, { encoding: 'utf-8' }, function(err, data) {
+                if (err) return reject(err);
+                console.log('received data: ' + data);
 
-      resolve(data);
-    });
-  })
-  .then(function(data) {
-    return db.runSql(data);
-  });
+                resolve(data);
+            });
+        })
+        .then(function(data) {
+            return db.runSql(data);
+        });
 };
 
 exports._meta = {
-  "version": 1
+    "version": 1
 };
