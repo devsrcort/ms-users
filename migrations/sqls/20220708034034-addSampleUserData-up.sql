96e3cdab807fc9929942d2f18728e34ce11e594c
Dev <dev@srcorp.io>
2022-07-08 12:57:51 +0900

    Update databases

---
+++ migrations/sqls/20220708034034-addSampleUserData-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+INSERT INTO `users` (`id`, `uuid`, `name`, `phonenumer`, `email`, `password`, `wallet_address`, `balance`,`withdraw_limit`,`privateKey`, `created`, `last_updated`)
+VALUES
+	(81, '6fc0f65e-c67a-4a15-811e-bd34e8e7ef5f', 'test21', '', 'test3432@example.com', '84fa9dd1a6b338c8347573ee76a16caf', '0x54c858B5E5c11A11095C74976E2A675734e7f9c6',10000,100,'','2017-03-20 01:28:20', '2017-04-18 01:30:58');
