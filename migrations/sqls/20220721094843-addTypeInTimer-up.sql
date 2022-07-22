68022dd5b2cd01023efd2c2d7c62a80cdf66eb80
Dev <dev@srcorp.io>
2022-07-22 18:16:38 +0900

    Update sync ratio

---
+++ migrations/sqls/20220721094843-addTypeInTimer-up.sql
@@ -0,0 +1,6 @@
+/* Replace with your SQL commands */
+ALTER TABLE `ddaytime` ADD 
+(
+    `sTimerType` varchar(100) NOT NULL DEFAULT '',
+    `sTimerStatus` varchar(100) NOT NULL DEFAULT ''
+)
--- test/services/timer-test.js
+++ test/services/timer-test.js
@@ -7,38 +7,72 @@ const log = require("metalogger")();
 const TimerService = require("users/services/timerService");
 
 describe("timer service test", () => {
-    const testService = new TimerService();
+  const testService = new TimerService();
 
-    it("timer Service - CRUD", async() => {
-        const ExpectedDate = new Date("2019-01-01T12:30:00.000Z");
-        const ExpectedUpdateDate = new Date("2020-01-01T12:30:00.000Z");
+  it("timer Service - CRUD", async () => {
+    const ExpectedDate = new Date("2019-01-01T12:30:00.000Z");
+    const ExpectedUpdateDate = new Date("2020-01-01T12:30:00.000Z");
 
-        await testService.addTimer("Test", "2019-01-01T12:30:00.000Z");
+    await testService.addTimer("Test", "2019-01-01T12:30:00.000Z");
 
-        let testTimer = await testService.getTimer("Test");
+    let testTimer = await testService.getTimer("Test");
 
-        assert.equal(
-            new Date(testTimer.slice(-1)[0]["destTime"]).valueOf(),
-            ExpectedDate.valueOf(),
-            `Date is not equals ${ExpectedDate}, ${new Date(
+    assert.equal(
+      new Date(testTimer.slice(-1)[0]["destTime"]).valueOf(),
+      ExpectedDate.valueOf(),
+      `Date is not equals ${ExpectedDate}, ${new Date(
         testTimer.slice(-1)[0]["destTime"]
       )}`
-        );
+    );
 
-        await testService.updateTimer("Test", "2020-01-01T12:30:00.000Z");
-        testTimer = await testService.getTimer("Test");
+    await testService.updateTimer("Test", "2020-01-01T12:30:00.000Z");
+    testTimer = await testService.getTimer("Test");
 
-        assert.equal(
-            new Date(testTimer.slice(-1)[0]["destTime"]).valueOf(),
-            ExpectedUpdateDate.valueOf(),
-            `Date is not equals ${ExpectedUpdateDate}, ${new Date(
+    assert.equal(
+      new Date(testTimer.slice(-1)[0]["destTime"]).valueOf(),
+      ExpectedUpdateDate.valueOf(),
+      `Date is not equals ${ExpectedUpdateDate}, ${new Date(
         testTimer.slice(-1)[0]["destTime"]
       )}`
-        );
+    );
 
-        testService.deleteTimer("Test");
+    await testService.deleteTimer("Test");
 
-        testTimer = await testService.getTimer("Test");
-        assert.equal(testTimer.length, 0, "testTimer is not length 0");
-    });
+    testTimer = await testService.getTimer("Test");
+    assert.equal(testTimer.length, 0, "testTimer is not length 0");
+  });
+
+  it("Timer Service - Sale Timer", async () => {
+    await testService.clearActiveSalesTimer();
+    const randomNumber = Math.floor(Math.random() * 13) + 2;
+    const ExpectedDate = new Date("2019-01-01T12:30:00.000Z");
+    const ExpectedName = `SaleTestTimer_${randomNumber}`;
+
+    await testService.addTimer(
+      ExpectedName,
+      "2019-01-01T12:30:00.000Z",
+      "Sale"
+    );
+    await testService.setActiveSaleTimer(ExpectedName);
+
+    const activeTimer = await testService.getActiveSaleTimer();
+
+    assert.equal(activeTimer.length, 1, `Just only one - Active Timer`);
+
+    assert.equal(
+      activeTimer[0]["name"].valueOf(),
+      ExpectedName,
+      `Name is not equals ${ExpectedName}, ${activeTimer[0]["name"]}`
+    );
+
+    assert.equal(
+      new Date(activeTimer[0]["destTime"]).valueOf(),
+      ExpectedDate.valueOf(),
+      `Date is not equals ${ExpectedDate}, ${new Date(
+        activeTimer[0]["destTime"]
+      )}`
+    );
+
+    await testService.deleteTimer(ExpectedName);
+  });
 });
